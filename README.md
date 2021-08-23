ad829f4ee26fc734546bad6a45d0bc676934e04b
Martin Infante <maleinf09@gmail.com>
2021-08-23 16:29:31 -0300

    Add log module

---
+++ README.md
@@ -0,0 +1,6 @@
+# Go program to keep track of resources usage and possible threads
+
+This Go program will keep track of different resources usage and will
+keep track of system logs to find events of interest and possible threads.
+
+## WIP...
---
+++ src/logger/logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import(
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+const(
+	layout string = "01-02-2006_03:04"
+)
+
+var debug_mode string = ""
+var log_chan chan string = make(chan string, 100)
+
+
+func write() {
+	log.Print(<-log_chan)
+}
+
+
+func SetLogFile(filepath string, debug string) int {
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+							 0666)
+	if err != nil {
+		return -1
+	}
+	log.SetOutput(file)
+	debug_mode = debug
+	return 0
+}
+
+
+func LogInfo(msg string, tag string) {
+	var dt string = time.Now().Format(layout)
+	var log_msg string = fmt.Sprintf("[%s - INFO - %s]: %s\n", tag, dt, msg)
+	log_chan <- log_msg
+	go write()
+	if debug_mode == "true" {
+		fmt.Printf("[%s- INFO - %s]: %s\n", tag, dt, msg)
+	}
+}
+
+
+func LogError(msg string, tag string) {
+	var dt string = time.Now().Format(layout)
+	var log_msg string = fmt.Sprintf("[%s - ERROR - %s]: %s\n", tag, dt, msg)
+	log_chan <- log_msg
+	go write()
+	if debug_mode == "true" {
+		fmt.Printf("[%s - ERROR - %s]: %s\n", tag, dt, msg)
+	}
+}
+
+
+func LogTestStep(step string) {
+	log.Printf(fmt.Sprintf("[TEST STEP]: %s---------------\n\n", step))
+	if debug_mode == "true" {
+		fmt.Printf(fmt.Sprintf("[TEST STEP]: %s---------------\n\n", step))
+	}
+}
